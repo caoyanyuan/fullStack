a5a3cc81dd4c61fd2d750d8e41c2708967ca6c2d
caoyanyuan <1689456277@qq.com>
2019-08-30 16:46:52 +0800

    node做简单的上传get，post，文件,没做出来。下次再来提交

--- node/server.js
+++ node/server.js
@@ -1,9 +1,19 @@
 const http = require('http')
 
 let server = http.createServer((req, res) => {
-    console.log('执行了')
-    res.write('aaa')
-    res.end()
+    let arr = []
+
+    req.on('data', data => {
+        console.log(data)
+        arr.push(data)
+    })
+    req.on('end', () => {
+        let data = Buffer.concat(arr)
+
+        res.end()
+    })
+
+
 })
 
 server.listen(8080)
