5506276b9fbf1e48de20b666fde8e206ccefee4c
caoyanyuan <1689456277@qq.com>
2019-09-02 12:51:36 +0800

    数据交互： post，get

--- node/server2.js
+++ node/server2.js
@@ -1,22 +1,24 @@
 const http = require('http')
-const url = require('url')
+const url = require('url')             //url.paser(整个url)
+const quertString = require('querystring')      //quertString.parse(url中的数据部分)
 
 // 数据交互
 let server = http.createServer((req, res) => {
     /** get方式
      * let {pathname, query} = url.parse(req.url, true)
-    
+
         console.log(pathname, query)
         res.end()
      */
-    
+
     // post
     let str = ""
     req.on('data', (data) => {
         str +=data
     })
     req.on('end', (data) => {
-        console.log(str)
+        let post= quertString.parse(str)
+        console.log(post)
     })
     res.end()
 })
